05852b7c4c5ed2a9a99de381ff0dc754ff4ce486
AnandSinhaProjects <anands.projects22@gmail.com>
2021-03-20 20:43:44 +0530

    New Exercise

--- Exercise/Exercise3.py
+++ Exercise/Exercise3.py
@@ -1,3 +1,10 @@
+""" 
+Question
+
+Write a function called norm that takes as input two Numpy column arrays A and B, 
+adds them, and returns s, the L2 norm of their sum.
+ """
+
 import numpy as np
 
 A = np.array([1,2,3,4])
@@ -5,3 +12,7 @@ B = A+4
 c = A+B
 s = np.linalg.norm(c)
 print(s)
+
+""" Output -->
+
+18.547236990991408 """
